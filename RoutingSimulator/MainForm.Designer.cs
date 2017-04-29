cc3d69c99a5b8320b95747f5b69abf2d2f61cc1e
DESKTOP\Sarunas <visarunas@gmail.com>
2017-04-29 20:21:17 +0300

    Initial commit

---
+++ RoutingSimulator/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RoutingSimulator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelGraphics = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelGraphics
+            // 
+            this.panelGraphics.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelGraphics.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panelGraphics.Location = new System.Drawing.Point(12, 199);
+            this.panelGraphics.Name = "panelGraphics";
+            this.panelGraphics.Size = new System.Drawing.Size(735, 354);
+            this.panelGraphics.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 565);
+            this.Controls.Add(this.panelGraphics);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelGraphics;
+    }
+}
+
---
+++ RoutingSimulator/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutingSimulator
+{
+    public class Node
+    {
+        public int Weight { get; set; }
+        public IList<Node> ConnectedNodes { get; set; }
+
+        public string Name { get; set; }
+
+
+
+    }
+}
